36c902470143237c1000d7d64bf54aff7ebbb32a
Adam <61239599+SAyZCZ@users.noreply.github.com>
2026-09-23 14:26:50 +0200

    Implement call statistics calculation
    Added functionality to read call statistics and calculate total and average calls.

--- Tasks_source_codes/Helpdesk_call_statistics.py
+++ Tasks_source_codes/Helpdesk_call_statistics.py
@@ -1,1 +1,20 @@
 
+# Read first_hour, second_hour, third_hour and fourth_hour.
+# Set hours_count and calculate total_calls and average_calls.
+# Print both results.
+
+first_hour = int(input())
+second_hour = int(input())
+third_hour = int(input())
+fourth_hour = int(input())
+
+hours_count = 4
+
+total_calls = first_hour + second_hour + third_hour + fourth_hour
+
+average_calls = total_calls / hours_count
+
+average_calls = average_calls
+
+print(total_calls)
+print(average_calls)
